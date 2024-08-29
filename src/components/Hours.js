43b35d4b494ffbd585b7f73ece4675ab8d54a061
SVterry2023 <126096350+SVterry2023@users.noreply.github.com>
2024-08-29 19:38:46 +0000

    reto 02.02

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,29 @@
 // Placeholder to host a component
+
+// components/Hours.js
+// components/Hours.js
+import React, { useEffect, useState } from 'react';
+
+const Hours = () => {
+  const [time, setTime] = useState(null);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const currentHours = currentDate.getHours();
+    const currentMinutes = currentDate.getMinutes();
+    setTime(`Son las ${currentHours}:${currentMinutes < 10 ? '0' : ''}${currentMinutes}`);
+  }, []);
+
+  if (!time) {
+    return null; // O puedes devolver un indicador de carga
+  }
+
+  return (
+    <div>
+      <h2>Hora actual:</h2>
+      <p>{time}</p>
+    </div>
+  );
+};
+
+export default Hours;
